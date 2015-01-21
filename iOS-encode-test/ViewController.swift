d03fad35ef3455ce3ec01dd9958244841df75f91
ludawei <dawei_lu@126.com>
2015-01-21 16:00:59 +0800

    Initial Commit

---
+++ iOS-encode-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-encode-test
+//
+//  Created by 卢大维 on 15/1/21.
+//  Copyright (c) 2015年 weather. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
